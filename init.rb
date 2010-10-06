bde04b67158a4bac3cc261bf5394a12e4dac8d37
Rico Sta. Cruz <rico@ambiescent.com>
2010-10-07 06:33:55 +0800

    Update comments.

--- init.rb
+++ init.rb
@@ -14,11 +14,10 @@ class Main < Sinatra::Base
   enable   :raise_errors
   use      Rack::Session::Cookie
 
-  # Load config
   Dir[root_path('config', '*.rb')].each { |f| load f unless f.include?('.example') }
 end
 
-# Bootstrap Aura.
+# Bootstrap Aura
 require './core/base/base'
 Aura::Extension.all.each { |ext| ext.load! }
 Aura::Models.unpack
