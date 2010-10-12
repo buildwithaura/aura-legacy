5153f816f6d04f48587be1c4270e3569f9927b7c
Rico Sta. Cruz <rico@ambiescent.com>
2010-10-12 16:41:59 +0800

    Add a todo note.

--- init.rb
+++ init.rb
@@ -4,6 +4,7 @@ require "rubygems"
 require "sinatra/base"
 require "rtopia"
 
+# TODO: Rename Main to Aura, and merge with class Aura
 class Main < Sinatra::Base
   set      :root, File.dirname(__FILE__)
   set      :root_path, lambda { |*args| File.join(root, *args) }
