d867e26de3a67e37e107d3675fe76e787de9ff7b
Rico Sta. Cruz <rico@ambiescent.com>
2010-10-14 00:37:28 +0800

    Fix sass_ext error.

--- core/base/lib/sass_ext.rb
+++ core/base/lib/sass_ext.rb
@@ -1,9 +1,12 @@
-module Sass::Script::Functions
-  def ie_gradient_filter(start_color, end_color)
-    assert_type start_color, :Color
-    assert_type end_color, :Color
-    s = "progid:DXImageTransform.Microsoft.gradient(startColorStr='#{start_color}', EndColorStr='#{end_color}')"
-    Sass::Script::String.new s, :identifier
+module Sass
+  module Script
+    module Functions
+      def ie_gradient_filter(start_color, end_color)
+        assert_type start_color, :Color
+        assert_type end_color, :Color
+        s = "progid:DXImageTransform.Microsoft.gradient(startColorStr='#{start_color}', EndColorStr='#{end_color}')"
+        Sass::Script::String.new s, :identifier
+      end
+    end
   end
 end
-
