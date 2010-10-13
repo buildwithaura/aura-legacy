88e16339ba505d450085f04fc0319e67e09f2737
Rico Sta. Cruz <rico@ambiescent.com>
2010-10-14 00:34:30 +0800

    Add IE gradients support.

---
+++ core/base/lib/sass_ext.rb
@@ -0,0 +1,9 @@
+module Sass::Script::Functions
+  def ie_gradient_filter(start_color, end_color)
+    assert_type start_color, :Color
+    assert_type end_color, :Color
+    s = "progid:DXImageTransform.Microsoft.gradient(startColorStr='#{start_color}', EndColorStr='#{end_color}')"
+    Sass::Script::String.new s, :identifier
+  end
+end
+
