4d10df7b6c7c2f593d945019553868d6dd2e39e3
Rico Sta. Cruz <rico@ambiescent.com>
2010-10-14 08:59:49 +0800

    Move settings stuff onto aura.rb.

--- core/base/lib/aura.rb
+++ core/base/lib/aura.rb
@@ -1,3 +1,21 @@
 class Aura
   Model = Sequel::Model
+
+  def self.get(key)
+    Models::Setting.find(:key => key.to_s).try(:value)
+  end
+
+  def self.set(key, value)
+    s = Models::Setting.find(:key => key.to_s) || Models::Setting.new
+    s.key   = key
+    s.value = value
+    s.save
+    value
+  end
+
+  def self.default(key, value)
+    s = Models::Setting.find(:key => key.to_s)
+    return set(key, value)  if s.nil?
+    get key
+  end
 end
--- core/base/models/settings.rb
+++ core/base/models/settings.rb
@@ -6,21 +6,4 @@ class Aura
       end
     end
   end
-
-  def self.get(key)
-    Models::Setting.find(:key => key.to_s).try(:value)
-  end
-
-  def self.set(key, value)
-    s = Model::Setting.find(:key => key.to_s) || Model::Setting.new
-    s.key   = key
-    s.value = value
-    s.save
-  end
-
-  def self.default(key, value)
-    s = Model::Setting.find(:key => key.to_s)
-    return set(key, value)  if s.nil?
-    get key
-  end
 end
