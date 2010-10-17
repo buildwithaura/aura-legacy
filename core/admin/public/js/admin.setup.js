eb954a6f93a915ff4ea0f914283b7824cd2d4a5f
Rico Sta. Cruz <rico@ambiescent.com>
2010-10-17 16:17:49 +0800

    Improve the HTML editor experience.

--- core/admin/public/js/admin.setup.js
+++ core/admin/public/js/admin.setup.js
@@ -18,7 +18,36 @@
   // Wysiwyg
   $("textarea.html, p.html textarea").livequery(function () {
     $(this).wysiwyg({
-      css: '/css/admin_wysiwyg_field.css'
+      css: '/css/admin_wysiwyg_field.css',
+      controls: {
+        strikeThrough: { visible: false },
+        subscript: { visible: false },
+        superscript: { visible: false },
+        separator03: { visible: false },
+        undo: { visible: false },
+        redo: { visible: false },
+        separator04: { visible: false },
+        cut: { visible: false },
+        copy: { visible: false },
+        paste: { visible: false },
+        html: {
+          visible: true,
+          exec: function() {
+            if (this.viewHTML) {
+              this.setContent($(this.original).val());
+              $(this.original).hide();
+              $(this.editor).show();
+            }
+            else {
+              this.saveContent();
+              $(this.editor).hide();
+              $(this.original).show().focus();
+            }
+
+            this.viewHTML = !( this.viewHTML );
+          }
+        }
+      }
     });
   });
 
