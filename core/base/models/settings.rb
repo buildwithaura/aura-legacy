cef59a9a3bc8e2187e668871c933da8f4f71b015
Rico Sta. Cruz <rico@ambiescent.com>
2010-10-17 21:31:28 +0800

    Expand settings to be anything (instead of just strings).

--- core/base/models/settings.rb
+++ core/base/models/settings.rb
@@ -1,6 +1,8 @@
 class Aura
   module Models
     class Setting < Model
+      plugin :serialization, :json, :value
+
       def self.seed(type=nil, &blk)
         Aura.default :'site.name', "My Site"
       end
