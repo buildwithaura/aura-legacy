df896e9ddb64b73847fc1aa828cda38ee90a2108
Rico Sta. Cruz <rico@ambiescent.com>
2010-10-20 16:52:46 +0800

    Add back cc_html as an unused helper.

--- core/base/helpers/main_helpers.rb
+++ core/base/helpers/main_helpers.rb
@@ -20,4 +20,18 @@ class Main
       end.join("\n")
     end
   end
+
+  # Implements the world-famous Paul Irish IE conditional comments HTML tag--in HAML.
+  # http://paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/
+  def cc_html(options={}, &blk)
+    attrs = options.map { |(k, v)| " #{h k}='#{h v}'" }.join('')
+    [ "<!--[if lt IE 7 ]> <html#{attrs} class='ie6'> <![endif]-->",
+      "<!--[if IE 7 ]>    <html#{attrs} class='ie7'> <![endif]-->",
+      "<!--[if IE 8 ]>    <html#{attrs} class='ie8'> <![endif]-->",
+      "<!--[if IE 9 ]>    <html#{attrs} class='ie9'> <![endif]-->",
+      "<!--[if (gt IE 9)|!(IE)]><!--> <html#{attrs}> <!--<![endif]-->",
+      capture_haml(&blk).strip,
+      "</html>"
+    ].join("\n")
+  end
 end
