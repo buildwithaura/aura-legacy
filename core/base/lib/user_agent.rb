3d8c63d64b6afd56c3ac982535bd26c16db7e289
Rico Sta. Cruz <rico@ambiescent.com>
2010-10-21 00:28:54 +0800

    More UA sniffies.

--- core/base/lib/user_agent.rb
+++ core/base/lib/user_agent.rb
@@ -21,10 +21,16 @@ class UserAgent
   # OS's and devices
   def linux?()     detail?(/^Linux/, 'Mozilla'); end
   def iphone?()    detail?(/^iPhone/, 'Mozilla'); end
+  def rim?()       detail?(/^Blackberry/, 'Mozilla'); end
+  def android?()   detail?(/^Android/, 'Mozilla'); end
+  def android1?()  detail?(/^Android 1\./, 'Mozilla'); end
+  def android2?()  detail?(/^Android 2\./, 'Mozilla'); end
+  def nokia?()     detail?(/^Nokia/, 'Mozilla') || detail?(/^Series[38]0/, 'Mozilla'); end
   def ipad?()      detail?(/^iPad/, 'Mozilla'); end
   def windows?()   detail?(/^Windows/, 'Mozilla'); end
   def osx?()       detail?(/^(Intel )?Mac OS X/, 'Mozilla'); end
   def mac?()       detail?(/^Macintosh/, 'Mozilla') || osx?; end
+  def blackberry?() detail?(/^Blackberry/, 'Mozilla'); end
 
   # IE
   def ie9?()       detail?('MSIE 9.0', 'Mozilla'); end
@@ -32,12 +38,31 @@ class UserAgent
   def ie7?()       detail?(/^MSIE 7.0/, 'Mozilla'); end
   def ie6?()       detail?(/^MSIE 6/, 'Mozilla'); end
 
+  # Different Firefox versions
+  def gecko40?()   gecko_version?(20100401, 20110101); end
+  def gecko36?()   gecko_version?(20091111, 20100401); end
+  def gecko35?()   gecko_version?(20090612, 20091111); end
+  def gecko30?()   gecko_version?(20080610, 20090612); end
+  def gecko20?()   gecko_version?(20061010, 20080610); end
+  def gecko_old?() gecko_version?(0, 20061010); end
+
+  def gecko_version?(from, to)
+    g = product('Gecko')
+    return nil  if g.nil?
+    v = g[:version][0...8].to_i
+    (v >= from) and (v < to)
+  end
+
   def to_s()       @ua_string; end
   def inspect()    @ua.inspect; end
 
   # Checks for, say, 'Gecko' in 'Gecko/3.9.82'
   def product?(str)
-    !! @ua.detect { |p| p[:product] == str }
+    !! product(str)
+  end
+
+  def product(str)
+    @ua.detect { |p| p[:product] == str }
   end
 
   # Checks for, say, 'MSIE' in 'Mozilla/5.0 (MSIE; x; x)'
@@ -48,7 +73,9 @@ class UserAgent
   end
 
   def body_class
-    (%w(webkit chrome safari ios gecko opera ie linux iphone) +
+    (%w(webkit chrome safari ios gecko opera ie linux) +
+     %w(blackberry nokia android iphone) +
+     %w(gecko36 gecko35 gecko30 gecko20 gecko_old) +
      %w(ipad windows osx mac ie6 ie7 ie8 ie9)).map do |aspect|
       aspect  if self.send :"#{aspect}?"
     end.compact.join(' ')
