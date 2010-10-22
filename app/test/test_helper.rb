07816ada742a3569af69695556a19a141bd596f6
Rico Sta. Cruz <rico@ambiescent.com>
2010-10-22 11:46:27 +0800

    Update tests.

--- app/test/test_helper.rb
+++ app/test/test_helper.rb
@@ -24,4 +24,25 @@ class Test::Unit::TestCase
   def app
     Main.new
   end
+
+  def p(obj)
+    return unless ENV['verbose']
+    str = obj.is_a?(String) ? obj : obj.inspect
+    file, line = p_source
+    basename = File.basename(file)
+
+    puts "\033[1;32m%20s\033[1;30m:%-3s\033[0m %s" % [basename, line, str]
+  end
+
+
+  # @return [Array] [ app/test/unit/migration_test.rb, 23 ]
+  def p_source
+    backtrace[2].split(':')[0..1]
+  end
+
+  def backtrace
+    raise 1
+  rescue => e
+    e.backtrace[2..-1]
+  end
 end
---
+++ app/test/unit/migration_test.rb
@@ -0,0 +1,42 @@
+require 'test_helper'
+
+class MigrationTest < Test::Unit::TestCase
+  setup do
+    @old = Page.schema
+  end
+
+  test "Should not remove old stuff" do
+    Page.sync_schema
+
+    Page.set_schema do
+      String :foobar
+    end
+
+    Page.sync_schema
+
+    p "Columns:"
+    p Page.columns
+    assert Page.columns.include? :foobar
+    assert Page.columns.include? :id
+    assert Page.columns.include? :parent_id
+
+    Page.set_schema do
+      String :meatballs
+    end
+
+    Page.sync_schema
+
+    p "Columns after modification:"
+    p Page.columns
+
+    assert Page.columns.include? :meatballs
+    assert Page.columns.include? :foobar
+    assert Page.columns.include? :id
+
+    # These should not throw errors
+    page = Page.new
+    page.id
+    page.parent_id
+    page.foobar
+  end
+end
