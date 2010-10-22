5be208826e33dc7c9dcb8022d3dc933fdc710db8
Rico Sta. Cruz <rico@ambiescent.com>
2010-10-22 22:45:55 +0800

    Change the js etag to an int.

--- core/admin/routes/admin.rb
+++ core/admin/routes/admin.rb
@@ -3,7 +3,7 @@ class Main
     js = settings.admin_js
     content_type :js
     last_modified js.mtime
-    etag js.mtime
+    etag js.mtime.to_i
     cache_control :public, :must_revalidate, :max_age => 86400*30
 
     js.compressed
