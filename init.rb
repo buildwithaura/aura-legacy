953365f854609ad6b4d2d75aa65d82535d3629b0
Rico Sta. Cruz <rico@ambiescent.com>
2010-10-23 09:07:02 +0800

    Implement the seed.yml standard.

--- init.rb
+++ init.rb
@@ -6,6 +6,7 @@ require "rtopia"
 require "sequel"
 require "sinatra/content_for"
 require "json"
+require "yaml"
 
 # TODO: Rename Main to Aura, and merge with class Aura
 class Main < Sinatra::Base
@@ -32,8 +33,21 @@ end
 # Bootstrap Aura
 require './app/main'
 
+# Load extensions
 Aura::Extension.active.each { |ext| ext.load! }
-Aura::Models.all.each { |m| m.seed }
+
+seed_file = Main.root_path('config', 'seed.yml')
+
+# If the file config/seed.yml is present, use that to initialize the DB.
+# Only if the DB is pristine, though!
+if File.exists?(seed_file) and !Aura::Models::Setting.table_exists?
+  Aura.db_restore YAML::load_file(seed_file)
+
+# ..otherwise, setup the database: do migrations and put in sample data.
+else
+  Aura::Models.all.each { |m| m.seed }
+end
+
 Aura::Models.unpack
 
 Aura::Extension.active.each { |ext| ext.init }
