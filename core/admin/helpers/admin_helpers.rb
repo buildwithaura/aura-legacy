e5d707e04fdd0f48b7e4043b431205b7afe6643e
Rico Sta. Cruz <rico@ambiescent.com>
2010-10-25 10:16:06 +0800

    Implement the admin_back_to_dashboard helper.

--- core/admin/helpers/admin_helpers.rb
+++ core/admin/helpers/admin_helpers.rb
@@ -1,5 +1,7 @@
 class Main
   module AdminHelpers
+    # Renders an admin page with the admin template.
+    # 
     def show_admin(template, locals={})
       show template, { :layout => :'admin/layout' }, locals
     end
@@ -13,9 +15,23 @@ class Main
       [{ :href => '/css/admin.css', :media => 'screen' }]
     end
 
+    # Draws an <img> tag of the given icon.
     def admin_icon(icon)
       icon = "#{icon}.png"  unless icon.to_s.include?('.')
       tag(:img, nil, { :src => "/images/admin_icons/#{icon}", :class => 'icon' })
+    end
+
+    # Renders a 'back to dashboard' link in the sidebar.
+    # Example:
+    #
+    #   - content_for :nav do
+    #     != admin_back_to_dashboard
+    #     %nav
+    #       %ul
+    #         ...
+    #
+    def admin_back_to_dashboard
+      partial :'admin/_back_to_dashboard'
     end
   end
 
