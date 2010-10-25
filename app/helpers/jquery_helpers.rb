72132c023d94f25f68f214e835a2804c21cb12a6
Rico Sta. Cruz <rico@ambiescent.com>
2010-10-25 21:33:16 +0800

    Make jquery_ui download from the CDN.

--- app/helpers/jquery_helpers.rb
+++ app/helpers/jquery_helpers.rb
@@ -3,10 +3,21 @@ class Main
     def jquery(path='/js/jquery.js', version='1.4.3')
       if settings.production?
         # Grab Google CDN's jQuery; fallback to local if necessary
-        "<script src=\"http://ajax.googleapis.com/ajax/libs/jquery/#{version}/jquery.min.js\"></script>"+
+        url = "http://ajax.googleapis.com/ajax/libs/jquery/#{version}/jquery.min.js"
+        "<script src=\"#{url}\"></script>"+
         "<script>!window.jQuery && document.write('<script src=\"#{path}\"><\\/script>')</script>"
       else
-        "<script src='#{path}'></script>"
+        "<script type='text/javascript' src='#{path}'></script>"
+      end
+    end
+
+    def jquery_ui(path='/js/jqueryui.js', version='1.8.5')
+      if settings.production?
+        url = "https://ajax.googleapis.com/ajax/libs/jqueryui/#{version}/jquery-ui.min.js"
+        "<script src=\"#{url}\"></script>"+
+        "<script>!window.jQuery.ui && document.write('<script src=\"#{path}\"><\\/script>')</script>"
+      else
+        "<script type='text/javascript' src='#{path}'></script>"
       end
     end
   end
