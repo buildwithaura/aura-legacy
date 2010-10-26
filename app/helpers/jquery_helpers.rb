ed460bc3ec69fba6edb02a78f9105fc7ef01c447
Rico Sta. Cruz <rico@ambiescent.com>
2010-10-26 10:36:36 +0800

    Update the JQuery helpers.

--- app/helpers/jquery_helpers.rb
+++ app/helpers/jquery_helpers.rb
@@ -4,7 +4,7 @@ class Main
       if settings.production?
         # Grab Google CDN's jQuery; fallback to local if necessary
         url = "http://ajax.googleapis.com/ajax/libs/jquery/#{version}/jquery.min.js"
-        "<script src=\"#{url}\"></script>"+
+        "<script type='text/javascript' src=\"#{url}\"></script>"+
         "<script>!window.jQuery && document.write('<script src=\"#{path}\"><\\/script>')</script>"
       else
         "<script type='text/javascript' src='#{path}'></script>"
@@ -13,8 +13,8 @@ class Main
 
     def jquery_ui(path='/js/jqueryui.js', version='1.8.5')
       if settings.production?
-        url = "https://ajax.googleapis.com/ajax/libs/jqueryui/#{version}/jquery-ui.min.js"
-        "<script src=\"#{url}\"></script>"+
+        url = "http://ajax.googleapis.com/ajax/libs/jqueryui/#{version}/jquery-ui.min.js"
+        "<script type='text/javascript' src=\"#{url}\"></script>"+
         "<script>!window.jQuery.ui && document.write('<script src=\"#{path}\"><\\/script>')</script>"
       else
         "<script type='text/javascript' src='#{path}'></script>"
