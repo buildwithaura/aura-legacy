e52794edf5e7f11a213dd06e0c41c78142f7319b
Rico Sta. Cruz <rico@ambiescent.com>
2010-10-28 00:55:44 +0800

    Attempt to get capybara working.

--- app/test/stories_helper.rb
+++ app/test/stories_helper.rb
@@ -1,19 +1,26 @@
-$stderr << "Stories don't work yet.\n"
-exit
-
 $:.unshift(*Dir['vendor/*/lib'])
 
+def require?(what, gem=what)
+  require what
+rescue LoadError
+  $stderr << "Oops! Testing needs the #{gem} gem. Please try:\n"
+  $stderr << "$ sudo gem install #{gem}\n"
+  exit
+end
+
 require "rubygems"
-require "contest"
-require "stories"
-require "stories/runner"
-require "capybara"
-require "capybara/dsl"
+require? "contest"
+require? "stories", :stories
+require? "stories/runner", :stories
+require? "capybara"
+require? "capybara/dsl", :capybara
 
 Capybara.configure do |c|
+  # In the future, if we want to do JS testing:
   # :culerity [headless], :selenium [firefox], :chrome
-  c.default_driver = (ENV['driver'] || $_driver || :chrome).to_sym
-  c.app_host       = ENV['host'] || $_host || 'http://localhost:4567'
+  # c.default_driver = (ENV['driver'] || $_driver || :chrome).to_sym
+  # c.app_host       = ENV['host'] || $_host || 'http://localhost:4567'
+  c.app = Main
 end
 
 Capybara.register_driver :chrome do |app|
@@ -23,4 +30,3 @@ end
 class Test::Unit::TestCase
   include Capybara
 end
-
