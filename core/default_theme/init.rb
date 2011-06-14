882aef83a71f27fc14acfe4a5d465ee5345d7dfc
Rico Sta. Cruz <rico@ambiescent.com>
2011-06-14 19:53:04 +0800

    .

--- core/default_theme/init.rb
+++ core/default_theme/init.rb
@@ -1,17 +1,15 @@
-if Aura::Models.const_defined?(:Page)
-  class Aura::Models::Page
-    subtype :portfolio,
-      :name     => "Portfolio page",
-      :template => "id_portfolio"
+class Aura::Models::Page
+  subtype :portfolio,
+    :name     => "Portfolio page",
+    :template => "id_portfolio"
 
-    custom_field :excerpt
+  custom_field :excerpt
 
-    form :portfolio do
-      text :excerpt, "Portfolio excerpt"
+  form :portfolio do
+    text :excerpt, "Portfolio excerpt"
 
-      #fieldset :extra do
-      #  text :'custom.excerpt', "Excerpt"
-      #end
-    end
+    #fieldset :extra do
+    #  text :'custom.excerpt', "Excerpt"
+    #end
   end
 end
